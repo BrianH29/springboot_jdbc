c7121f77fe62f58329a2f5fbbd34caf6ee74a2aa
BrianH <brianhwang29@gmail.com>
2021-07-06 13:30:35 +0900

    modified

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# springboot_jdbc
+### springboot_jdbc
+reference 가장 빨리만나는 스프링 부트 - MAKI TOSIAKI
--- src/main/java/com/bh/jdbc/JdbcApplication.java
+++ src/main/java/com/bh/jdbc/JdbcApplication.java
@@ -1,32 +1,27 @@
 package com.bh.jdbc;
 
 import com.bh.jdbc.domain.Customer;
+import com.bh.jdbc.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class JdbcApplication implements CommandLineRunner {
 
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 
 	@Override
 	public void run(String... strings) throws Exception {
-		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		SqlParameterSource param = new MapSqlParameterSource()
-				.addValue("id", 1);
+		//데이터 추가
+		Customer created = customerRepository.save(new Customer(null, "Jordan","Kwang"));
+		System.out.println("created : " + created);
 
-		Customer result = jdbcTemplate.queryForObject(sql, param,
-				(rs, rowNum) -> new Customer(rs.getInt("id"),
-						rs.getString("first_name"), rs.getString("last_name"))
-		);
-
-		System.out.println("result = " + result);
+		//데이터 표시
+		customerRepository.findAll().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
